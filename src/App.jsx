7bb4a525b435d037d0a7823050765a7264753a6a
chaitanya-sai-neeli <chaitanyasaichaitu123@gmail.com>
2025-02-21 20:53:20 +0530

    Created project and added Sidbar component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SideBar from "./Components/Sidebar/SideBar";
+
+const App = () => {
+  return (
+    <div>
+      <SideBar />
+    </div>
+  );
+};
+
+export default App;

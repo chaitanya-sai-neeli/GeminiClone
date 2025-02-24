44ae0344dda11e17c6574c656f23e2301145f143
chaitanya-sai-neeli <chaitanyasaichaitu123@gmail.com>
2025-02-24 16:17:49 +0530

    Added Main component and its CSS

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -8,6 +8,32 @@ const Main = () => {
         <p>Gemini</p>
         <img src={assets.user_icon} alt="" />
       </div>
+      <div className="main-container">
+        <div className="greet">
+          <p>
+            <span>Hello, Dev</span>
+          </p>
+          <p>How can I help you today ?</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Suggest some trekking spots in Telangana</p>
+            <img src={assets.compass_icon} alt="compass pic" />
+          </div>
+          <div className="card">
+            <p>Briefly summarize this concept: Urban Planning</p>
+            <img src={assets.bulb_icon} alt="compass pic" />
+          </div>
+          <div className="card">
+            <p>Brain storm team bonding activities for our work retreat</p>
+            <img src={assets.message_icon} alt="compass pic" />
+          </div>
+          <div className="card">
+            <p>Improve the readability of the following code</p>
+            <img src={assets.code_icon} alt="compass pic" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };

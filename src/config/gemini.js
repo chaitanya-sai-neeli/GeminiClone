31d2082d857bf9209219b2d271a29ebb2bc92378
chaitanya-sai-neeli <chaitanyasaichaitu123@gmail.com>
2025-03-06 14:04:55 +0530

    Added correct syntax for using .env in react+vite project

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,11 +1,10 @@
-const {
+import {
   GoogleGenerativeAI,
   HarmCategory,
   HarmBlockThreshold,
-} = require("@google/generative-ai");
+} from "@google/generative-ai";
 
-// const apiKey = process.env.GEMINI_API_KEY;
-const apiKey = process.env.REACT_APP_GEMINI_API_KEY;
+const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
